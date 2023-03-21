bf29b26987d9404dc1f3d7833ac637b17df5a8fc
mkrolczyk12 <m.krolczyk66@gmail.com>
2023-03-21 21:35:45 +0100

    mkrolczyk - set up OpenAPIDefinition LOCAL env hosts for auth service

--- auth/src/main/java/pl/quiz/up/auth/AuthApplication.java
+++ auth/src/main/java/pl/quiz/up/auth/AuthApplication.java
@@ -1,6 +1,7 @@
 package pl.quiz.up.auth;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -11,7 +12,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("pl.quiz.up.auth.*")
 @ComponentScan(basePackages = {"pl.quiz.up.auth.*"})
 @EntityScan("pl.quiz.up.auth.*")
-@OpenAPIDefinition
+@OpenAPIDefinition(
+    servers = {
+        @Server(url = "http://localhost:8081/auth", description = "[LOCAL env] API Gateway service auth service endpoint"),
+        @Server(url = "http://localhost:8181/auth", description = "[LOCAL env] Auth service direct endpoint (Default: DISABLED)")})
 public class AuthApplication {
 
     public static void main(String[] args) {
