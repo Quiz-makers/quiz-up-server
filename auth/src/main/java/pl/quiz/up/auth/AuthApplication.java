c93d0a53efcff13b24c6cc74df4b141fb016b008
mkrolczyk12 <m.krolczyk66@gmail.com>
2023-04-03 19:25:10 +0200

    mkrolczyk - updated docker-compose, configured gateway-service and swagger for quiz-service

--- auth/src/main/java/pl/quiz/up/auth/AuthApplication.java
+++ auth/src/main/java/pl/quiz/up/auth/AuthApplication.java
@@ -8,8 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(scanBasePackages = {"pl.quiz.up.auth.*", "pl.quiz.up.common.*"})
 @OpenAPIDefinition(
         servers = {
-                @Server(url = "http://localhost:8081/auth", description = "[LOCAL env] API Gateway service auth service endpoint"),
-                @Server(url = "http://localhost:8181/auth", description = "[LOCAL env] Auth service direct endpoint (Default: DISABLED)")})
+                @Server(url = "http://localhost:8081/auth", description = "[LOCAL env] API Gateway auth service endpoint"),
+                @Server(url = "http://localhost:8190/auth", description = "[LOCAL env] Auth service direct endpoint (Default: DISABLED)")})
 public class AuthApplication {
 
     public static void main(String[] args) {
