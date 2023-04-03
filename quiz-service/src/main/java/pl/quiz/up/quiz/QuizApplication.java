5e03baba91839e7980d0bbbc728f0aea0f79af12
Marcin Królczyk <61460809+mkrolczykk@users.noreply.github.com>
2023-04-03 19:27:07 +0200

    mkrolczyk - updated docker-compose, configured gateway-service and swagger for quiz-service (#7)

--- quiz-service/src/main/java/pl/quiz/up/quiz/QuizApplication.java
+++ quiz-service/src/main/java/pl/quiz/up/quiz/QuizApplication.java
@@ -1,9 +1,15 @@
 package pl.quiz.up.quiz;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {"pl.quiz.up.quiz.*", "pl.quiz.up.common.*"})
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "http://localhost:8081/quiz", description = "[LOCAL env] API Gateway quiz service endpoint"),
+                @Server(url = "http://localhost:8290/quiz", description = "[LOCAL env] Quiz service direct endpoint (Default: DISABLED)")})
 public class QuizApplication {
     public static void main(String[] args) {
         SpringApplication.run(QuizApplication.class, args);
